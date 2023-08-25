7da28f8d9ed18ec7dccb72897a4863588c25e0b1
Matthieumoto <136125610+Matthieumoto@users.noreply.github.com>
2023-08-26 00:01:06 +0200

    3

--- script.js
+++ script.js
@@ -1,26 +1,69 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const addToCartButtons = document.querySelectorAll('.add-to-cart');
-    const cartItems = [];
-  
+// Fonction pour ajouter un article au panier
+function addToCart(name, price, imageSrc) {
+    const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+    cartItems.push({ name, price, imageSrc });
+    localStorage.setItem('cart', JSON.stringify(cartItems));
+}
+
+// Au clic sur le bouton "Ajouter au panier"
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
 addToCartButtons.forEach(button => {
-    button.addEventListener('click', function () {
-        const productName = this.getAttribute('data-name');
-        const productPrice = parseFloat(this.getAttribute('data-price'));
-  
-        cartItems.push({ name: productName, price: productPrice });
-        updateCartDisplay();
+    button.addEventListener('click', () => {
+        const itemName = button.getAttribute('data-name');
+        const itemPrice = button.getAttribute('data-price');
+        const itemImageSrc = button.parentNode.querySelector('.image').getAttribute('src');
+        addToCart(itemName, itemPrice, itemImageSrc);
     });
 });
-  
-function updateCartDisplay() {
-    const cartContainer = document.getElementById('cart-items');
-    cartContainer.innerHTML = '';
-  
+
+// Fonction pour afficher les articles du panier dans la page panier.html
+function displayCartItems() {
+    const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+    const cartItemsContainer = document.getElementById('cart-items');
+
+    cartItemsContainer.innerHTML = '';
+
     cartItems.forEach(item => {
-        const cartItemDiv = document.createElement('div');
-        cartItemDiv.innerHTML = `${item.name} - ${item.price}€`;
-        cartContainer.appendChild(cartItemDiv);
+        const cartItemElement = document.createElement('div');
+        cartItemElement.innerHTML = `<p>${item.name} - ${item.price}</p>`;
+        cartItemsContainer.appendChild(cartItemElement);
     });
-    }
+}
+
+// Au chargement de la page panier.html, afficher les articles du panier
+window.addEventListener('load', () => {
+    displayCartItems();
 });
-  
+
+// Au chargement de n'importe quelle page, mettre à jour le contenu du panier dans le menu
+window.addEventListener('load', () => {
+    const cartItemsCount = (JSON.parse(localStorage.getItem('cart')) || []).length;
+    const cartMenu = document.getElementById('cart-menu');
+    cartMenu.textContent = `Panier (${cartItemsCount})`;
+});
+
+function displayCartItems() {
+    const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+    const cartItemsContainer = document.getElementById('cart-items');
+
+    cartItemsContainer.innerHTML = '';
+
+    cartItems.forEach(item => {
+        const cartItemElement = document.createElement('div');
+        cartItemElement.classList.add('cart-item');
+
+        const itemImage = document.createElement('img');
+        itemImage.src = item.imageSrc;
+        itemImage.alt = item.name;
+        itemImage.classList.add('cart-item-image');
+
+        const itemName = document.createElement('p');
+        itemName.textContent = item.name;
+        itemName.classList.add('cart-item-name');
+
+        cartItemElement.appendChild(itemImage);
+        cartItemElement.appendChild(itemName);
+
+        cartItemsContainer.appendChild(cartItemElement);
+    });
+}
